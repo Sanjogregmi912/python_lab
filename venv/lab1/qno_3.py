63df864904e9989235cbdb5701807a462c79feff
Sanjogregmi912 <78246727+Sanjogregmi912@users.noreply.github.com>
2021-02-23 12:23:06 +0545

    initial commit

--- venv/lab1/qno_3.py
+++ venv/lab1/qno_3.py
@@ -1,1 +1,7 @@
-#N students take  K apples and distribute them among each other evenly. the remaining (the undivisible)part reamins in the basket .how many apples will each single students get? how many apples will remain in the basket? the program reads
+# N students take  K apples and distribute them among each other evenly. the remaining (the undivisible)part reamins in the basket .how many apples will each single students get? how many apples will remain in the basket? the program reads the number N and K. It should print the two answer for question above
+N=int(input("enter the number of students:"))
+K=int(input("enter the number of apples:"))
+t=K//N
+n=K%N
+print(f"the number of apples that students gets: {t}")
+print(f"the number of remaining apples: {n}")
