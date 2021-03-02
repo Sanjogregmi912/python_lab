11eb64bde42a51446e0114aeb56e3fc24899aa84
Sanjogregmi912 <78246727+Sanjogregmi912@users.noreply.github.com>
2021-03-02 12:24:28 +0545

    initial commit

---
+++ qn_6.py
@@ -0,0 +1,8 @@
+#solve each of the followingproblems using python scripts.make sure you use appropriate variabke name sand comments.
+#when there is final answer have pyhton print it to the screen
+#a person body mass index(bmi)is defined as:
+#BMI= mass in kg / height in m**2
+mass=float(input("enter the mass (in kg):"))
+height=float(input("Enter the height in meter square:"))
+BMI= mass//height
+print(f"the body mass index of any person is {BMI} in kg per meter square")
