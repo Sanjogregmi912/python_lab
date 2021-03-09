566ee2193dd512d301ae4802f2c7cf0b3e8c097b
Sanjogregmi912 <78246727+Sanjogregmi912@users.noreply.github.com>
2021-03-09 11:58:06 +0545

    initial commit

---
+++ lab_excerise/qno 8.py
@@ -0,0 +1,5 @@
+#write a python program which accepts the radius of a circle from the user and compute the area (area of circle = pier**2
+radius=float(input("Enter the value of radius:"))
+pie= 22/7
+area = pie*radius**2
+print(f"the area of circle is {area}")
